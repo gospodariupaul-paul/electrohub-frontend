cb425e4ad5c20ba85b6c5763bebed52da17ffd47
gospodariupaul-paul <gospodariupaul@gmail.com>
2026-02-14 09:08:42 +0200

    Update categories.ts

--- app/services/categories.ts
+++ app/services/categories.ts
@@ -1,11 +1,14 @@
-import axios from "axios";
-
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
 export async function getCategories() {
   try {
-    const response = await axios.get(`${API_URL}/categories`);
-    return response.data;
+    const response = await fetch("http://localhost:5000/api/categories", {
+      cache: "no-store",
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch categories");
+    }
+
+    return await response.json();
   } catch (error) {
     console.error("Error fetching categories:", error);
     return [];

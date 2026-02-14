b9eacd5f257953ae117a81c285fffaf32b24452a
Paul-Stelian <paulstelian@example.com>
2026-02-14 22:09:32 +0200

    Fix axios baseURL

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://electrohub-backend-1-10qa.onrender.com/api",
+  baseURL: "https://electrohub-backend-1-10qa.onrender.com"
   withCredentials: true,
 });
 

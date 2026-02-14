364b68ddb36fe23f44222fb491e9df27e8b25d59
Paul-Stelian <paulstelian@example.com>
2026-02-14 22:12:54 +0200

    Fix login cookie issue

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://electrohub-backend-1-10qa.onrender.com"
+  baseURL: "https://electrohub-backend-1-10qa.onrender.com",
   withCredentials: true,
 });
 

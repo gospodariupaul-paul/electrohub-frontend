7e4ef550c92603e849151b7dae226e06e38f9bda
Paul-Stelian <paulstelian@example.com>
2026-02-15 18:33:53 +0200

    Fix NextAuth types for role

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -2,16 +2,27 @@ import NextAuth from "next-auth";
 
 declare module "next-auth" {
   interface User {
-    access_token?: string;
+    id: string;
+    email: string;
+    name: string;
+    role: string; // ← ADĂUGAT
   }
 
   interface Session {
-    accessToken?: string;
+    user: {
+      id: string;
+      email: string;
+      name: string;
+      role: string; // ← ADĂUGAT
+    };
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
-    accessToken?: string;
+    id: string;
+    email: string;
+    name: string;
+    role: string; // ← ADĂUGAT
   }
 }

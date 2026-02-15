3a85fb1e09ac6de5b472cfdbf682f0224a697f24
Paul-Stelian <paulstelian@example.com>
2026-02-15 18:29:54 +0200

    Fix NextAuth types for access_token

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,28 +1,17 @@
-import NextAuth, { DefaultSession } from "next-auth";
+import NextAuth from "next-auth";
 
 declare module "next-auth" {
   interface User {
-    id: string;
-    name: string;
-    email: string;
-    role?: string;
+    access_token?: string;
   }
 
   interface Session {
-    user: {
-      id: string;
-      name: string;
-      email: string;
-      role?: string;
-    } & DefaultSession["user"];
+    accessToken?: string;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
-    id: string;
-    name: string;
-    email: string;
-    role?: string;
+    accessToken?: string;
   }
 }

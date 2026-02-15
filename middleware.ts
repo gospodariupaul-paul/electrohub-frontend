98aff2d7494e3057b959023b299adf11f8c8ce3b
Paul-Stelian <paulstelian@example.com>
2026-02-15 16:05:53 +0200

    Fix middleware for NextAuth v4 on Next.js 15

---
+++ middleware.ts
@@ -0,0 +1,10 @@
+export { default } from "next-auth/middleware";
+
+export const config = {
+  matcher: [
+    "/dashboard/:path*",
+    "/products/:path*",
+    "/categories/:path*",
+    "/users/:path*",
+  ],
+};

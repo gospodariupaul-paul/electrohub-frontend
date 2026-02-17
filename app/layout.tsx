2620fb09335507a355bc3c464e9f4fce103eba3d
Paul-Stelian <paulstelian@example.com>
2026-02-17 20:18:23 +0200

    add Providers and fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { SessionProvider } from "next-auth/react";
+import { Providers } from "./providers";
 
 export const metadata: Metadata = {
   title: "GOSPO Electro Hub",
@@ -22,9 +22,7 @@ export default function RootLayout({
       </head>
 
       <body className="antialiased">
-        <SessionProvider>
-          {children}
-        </SessionProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );

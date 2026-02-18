4b13fb6bead12ca93a6cb2b7b0e014a623343ef4
Paul-Stelian <paulstelian@example.com>
2026-02-18 02:24:27 +0200

    fix: replaced dashboard pages with clean working versions

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,16 +1,3 @@
-import { getUsers } from "@/app/services/users";
-import UsersClient from "./UsersClient";
-
-export default async function UsersPage() {
-  const users = await getUsers();
-
-  return (
-    <div style={{ padding: 30 }}>
-      <h1 style={{ fontSize: 28, marginBottom: 20 }}>Users</h1>
-      <UsersClient users={users} />
-    </div>
-  );
-}
 "use client";
 
 import { useEffect, useState } from "react";

36b6c6541ada942a1fb766369e3cc0cf9799d070
Paul-Stelian <paulstelian@example.com>
2026-02-24 01:46:14 +0200

    Fix login URL + use client position

--- app/login/LoginForm.tsx
+++ app/login/LoginForm.tsx
@@ -1,6 +1,6 @@
-import Link from "next/link";
 "use client";
 
+import Link from "next/link";
 import { useState } from "react";
 import { FaEnvelope, FaLock, FaGoogle, FaGithub } from "react-icons/fa";
 import { useRouter } from "next/navigation";

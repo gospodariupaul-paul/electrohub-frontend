bc56d30235cdc74ea780a0ac616d00fbc2c75fde
Paul-Stelian <paulstelian@example.com>
2026-03-11 08:58:24 +0200

    Fix search API URL (api/products/search)

--- app/search/SearchContent.tsx
+++ app/search/SearchContent.tsx
@@ -15,8 +15,10 @@ export default function SearchContent() {
     const load = async () => {
       setLoading(true);
 
+      const API = process.env.NEXT_PUBLIC_API_URL;
+
       const res = await axios.get(
-        `https://electrohub-backend-1-10qa.onrender.com/api/products/search?q=${q}`
+        `${API}/api/products/search?q=${q}`
       );
 
       setResults(res.data);
